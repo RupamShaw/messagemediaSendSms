b55633b447c950f9b93d696965a5ebaae646e37a
Glitch (knowing-den) <none>
2017-11-23 03:49:29 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -71,9 +71,11 @@ request.post('https://api.messagemedia.com/v1/messages', {
   if (response.statusCode < 300){
     res.send("OK");
     console.log(body.messages[0].message_id);
+    setTimeout(function(){
+    console.log("THIS IS");
+    }, 2000);
     request.get('https://api.messagemedia.com/v1/messages/'+body.messages[0].message_id,  {
   json : true,
-  
   auth : {
     username : process.env.USERNAME,
     password : process.env.PASSWORD
