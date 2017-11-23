7dd530d6cbc96411454d6b17873e6c06c7aa3539
Glitch (knowing-den) <none>
2017-11-23 04:11:29 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -7,38 +7,7 @@ var express = require('express');
 var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-// // http://expressjs.com/en/starter/static-files.html
-// app.use(express.static('public'));
-
-// // http://expressjs.com/en/starter/basic-routing.html
-// app.get("/", function (request, response) {
-//   response.sendFile(__dirname + '/views/index.html');
-// });
-
-// app.get("/dreams", function (request, response) {
-//   response.send(dreams);
-// });
-
-// // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-// app.post("/dreams", function (request, response) {
-//   dreams.push(request.query.dream);
-//   response.sendStatus(200);
-// });
-
-// // Simple in-memory store for now
-// var dreams = [
-//   "Find and count some sheep",
-//   "Climb a really tall mountain",
-//   "Wash the dishes"
-// ];
-
-// // listen for requests :)
-// var listener = app.listen(process.env.PORT, function () {
-//   console.log('Your app is listening on port ' + listener.address().port);
-// });
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended : true }));
@@ -81,8 +50,9 @@ request.post('https://api.messagemedia.com/v1/messages', {
         password : process.env.PASSWORD
       }
     },function (request, response) {
-        console.log(response.body.status);
+        console.log('status: '+response.body.status);
         res.send("response.body.status");
+        res.write("<p>Hello World</p>");
       });
   }else
     res.send("Error from API: " + body);
