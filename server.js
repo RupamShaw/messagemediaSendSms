7b616726b281a685ada553232ef27adf754fe1a4
Glitch (knowing-den) <none>
2017-11-27 01:48:10 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ var btoa = require('btoa');
 var app = express();
 app.use(bodyParser.urlencoded({ extended : true }));
 app.use(cors());
+
+
 // app.use(basicAuth({
 //     users: { "BX6fwDQToAwiQtEGmAB4": process.env.PASSWORD },
 //   unauthorizedResponse: getUnauthorizedResponse
@@ -36,12 +38,23 @@ messages.push({
   content : message,
   destination_number : numbers[n],
   callback_url: 'https://requestb.in/18hajq61'
-  
   });
 }
-  console.log("*********messages");
+request.post('https://api.messagemedia.com/v1/messages', {
+  json : true,
+  body : { messages : messages },
+  auth : {
+    username : process.env.USERNAME,    password : process.env.PASSWORD
+  }
+}, function(error, response, body) {
+  res.send('status :   '+response.body.status);
+  console.log(error);
+  console.log(response);
+});  
+
+/*  console.log("*********messages");
   console.log(messages);
-     var credentials =  btoa("BX6fwDQToAwiQtEGmAB4" + ':' +"zhdRDol9A9Wlzwy1SqkESWRMtuHUpL" );
+  var credentials =  btoa("BX6fwDQToAwiQtEGmAB4" + ':' +"zhdRDol9A9Wlzwy1SqkESWRMtuHUpL" );
   let encrypted = credentials.toString('base64');
   let BasicAuth = 'Basic ' + encrypted;
    
@@ -59,10 +72,10 @@ messages.push({
     //         'Accept': 'application/json',
     //         "Access-Control-Allow-Credentials":"true"
         },
-        auth: {
-          username: "BX6fwDQToAwiQtEGmAB4",
-          password: "zhdRDol9A9Wlzwy1SqkESWRMtuHUpL"
-        },
+        // auth: {
+        //   username: "BX6fwDQToAwiQtEGmAB4",
+        //   password: "zhdRDol9A9Wlzwy1SqkESWRMtuHUpL"
+        // },
         responseType: 'json', // default
         
       // method: 'post',
@@ -81,7 +94,7 @@ messages.push({
       console.log(error)
   });
  
-  
+*/  
 /*request.post('https://api.messagemedia.com/v1/messages', {
   json : true,
   body : { messages : messages },
